ec2eb8904323c6305a82aef7760eb31220613b91
lims <lim9809031@gmail.com>
2025-07-09 11:37:59 +0900

    DB 최적화

--- db/connection.py
+++ db/connection.py
@@ -6,7 +6,7 @@ from config import DB_USER, DB_PASSWORD, DB_HOST, DB_NAME
 DB_URL = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}"
 
 
-engine = create_engine(DB_URL, echo=True)
+engine = create_engine(DB_URL, echo=True, pool_size=10, max_overflow=20, pool_timeout=30)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 # ✅ 요게 필요했던 부분!
